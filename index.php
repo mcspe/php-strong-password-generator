bfb692b163c928e10e296752123ef978c20b7ea3
mcspe <mcspezzaferro@gmail.com>
2023-05-17 15:37:37 +0200

    [add] milestone 2

--- index.php
+++ index.php
@@ -1,45 +1,14 @@
 <?php
-  function randPwd($lenght) {
-    // set possible characters
-    $characters = [
-      'abcdefghijklmnopqrstuvwxyz',
-      '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~',
-      '0123456789'
-    ];
-    $characters[] = strtoupper($characters[0]);
-    
-    // set provisional password var
-    $provisionalPwd = '';
 
-    // declare random character selector function
-    function getRandChar($str) {
-      $index = rand(0, (strlen($str) - 1));
-      return $str[$index];
-    }
+  require_once __DIR__ . '/randpwd.php';
 
-    // add mandatory charachters 
-    for ($i=0; $i < count($characters); $i++) { 
-      $provisionalPwd .= getRandChar($characters[$i]);
-    }
-
-    // add to provisional password as many random charachters as the total lenght needed minus the mandatory charachters
-    for ($j=0; $j < ($lenght - 4); $j++) { 
-      //select a random index to add a letter, a number, a special character or a capital letter
-      $charTypeSelector = rand(0, (count($characters) - 1)); 
-      $provisionalPwd .= getRandChar($characters[$charTypeSelector]);
-    }
-
-    // randomize the pwd generated to avoid the first 4 characters of the string to always be in the same position
-    $pwd = str_shuffle($provisionalPwd);
-
-    return $pwd;
-  }
   $showResult = false;
 
   if (isset($_GET) && !empty($_GET['pwdLength'])) {
     $result = randPwd($_GET['pwdLength']);
     $showResult = true;
   }
+  
 ?>
 
 <!DOCTYPE html>

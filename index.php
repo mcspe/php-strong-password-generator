e8abc5afd8b87c380f802b6955b5a6068d000799
mcspe <mcspezzaferro@gmail.com>
2023-05-17 14:37:48 +0200

    [add] form

--- index.php
+++ index.php
@@ -12,6 +12,26 @@
   <title>PHP Strong Password Generator</title>
 </head>
 <body>
-  
+  <header class="container-fluid bg-dark text-center text-white py-3">
+    <h1>Password Generator</h1>
+  </header>
+  <main>
+    <div class="container my-5">
+      <form action="index.php" method="GET">
+        <div class="row justify-content-center">
+          <div class="form-floating col-7 m-3">
+            <input type="number" class="form-control" name="pwdLength" id="floatingInput" min="8" max="16" step="1" placeholder="Insert a number between 8 and 16 to generate a related length password">
+            <label for="floatingInput">Insert a number between 8 and 16 to generate a related length password</label>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-2">
+            <button type="submit" class="btn btn-warning">GENERATE</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </main>
+  <footer></footer>
 </body>
 </html>
